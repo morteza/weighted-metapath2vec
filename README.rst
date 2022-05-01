584680688870fa1937bf8fd5fa2162d6a3874462
Morteza Ansarinia <ansarinia@me.com>
2022-05-01 12:04:30 +0200

    initial commit of a poetry project template

---
+++ README.rst
@@ -0,0 +1,3 @@
+# Weighted Metapath2vec
+
+Python package for a weighted variant of metapath2vec embedding.
---
+++ tests/test_wmp2vec.py
@@ -0,0 +1,5 @@
+from wmetapath2vec import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
