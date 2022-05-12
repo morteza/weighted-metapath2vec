bc56a80152c71a0a3db49e096443043de044a668
Morteza Ansarinia <ansarinia@me.com>
2022-05-12 08:26:49 +0200

    refactor tests

--- tests/test_random_walk.py
+++ tests/test_random_walk.py
@@ -1,6 +1,24 @@
 import numpy as np
 import networkx as nx
-from wmetapath2vec.random_walk import nx_random_walk, weighted_random_walk
+from wmetapath2vec.random_walk import nx_random_walk, weighted_random_walk, match_metapath
+
+
+def test_match_metapath():
+
+    weights = np.random.rand(5, 5)
+    np.fill_diagonal(weights, 0)
+
+    G = nx.from_numpy_array(weights)
+    G.nodes[0]['type'] = 'task'
+    G.nodes[1]['type'] = 'task'
+    G.nodes[2]['type'] = 'construct'
+    G.nodes[3]['type'] = 'construct'
+    G.nodes[4]['type'] = 'construct'
+    # G[0][2]['weight'] = 10
+
+    metapaths = [['task', 'construct', 'task']]
+
+    assert match_metapath(G, [0, 2, 1, 4, 1], metapaths)
 
 
 def test_weighted_random_walk():
